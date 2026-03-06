f0caa73cdbf884ceaf9892006df15df75615d4c6
He SUN <hehe2700@HedeMacBook-Air.local>
2026-03-06 17:13:56 +0800

    hw02 finished

--- HW/hw02/hw02.py
+++ HW/hw02/hw02.py
@@ -32,6 +32,10 @@ def product(n, term):
     162
     """
     "*** YOUR CODE HERE ***"
+    num = 1
+    for i in range(1, n + 1):
+        num *= term(i)
+    return num
 
 
 def accumulate(fuse, start, n, term):
@@ -54,6 +58,11 @@ def accumulate(fuse, start, n, term):
     19
     """
     "*** YOUR CODE HERE ***"
+    total, k = start, 1
+    while k <= n:
+        total, k = fuse(total, term(k)), k + 1
+    return total
+            
 
 
 def summation_using_accumulate(n, term):
@@ -68,7 +77,7 @@ def summation_using_accumulate(n, term):
     >>> [type(x).__name__ for x in ast.parse(inspect.getsource(summation_using_accumulate)).body[0].body]
     ['Expr', 'Return']
     """
-    return ____
+    return accumulate(add, 0, n, term)
 
 
 def product_using_accumulate(n, term):
@@ -83,7 +92,7 @@ def product_using_accumulate(n, term):
     >>> [type(x).__name__ for x in ast.parse(inspect.getsource(product_using_accumulate)).body[0].body]
     ['Expr', 'Return']
     """
-    return ____
+    return accumulate(mul, 1, n , term)
 
 
 def make_repeater(f, n):
@@ -100,4 +109,11 @@ def make_repeater(f, n):
     390625
     """
     "*** YOUR CODE HERE ***"
+    def repeater(x):
+        for i in range(n):
+            x = f(x)
+        return x
+    return repeater
+ 
+
 
